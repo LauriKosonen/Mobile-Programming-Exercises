415973e0710b768f88f832175dd140a3b39e8b5c
Lauri Kosonen <AB7344@student.jamk.fi>
2023-09-15 10:30:35 +0300

    movies done untill Modify MovieListItems creation

---
+++ Movies/MovieListScreen.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image
+} from 'react-native';
+
+function MoviesList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.themoviedb.org/3/movie/now_playing?api_key=5e000196349daaec4dbdd97808666248&append_to_response=videos')
+      .then(response => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      })
+  }, []);
+
+  if (movies.length === 0) {
+    return (
+      <View style={{ flex: 1, padding: 20 }}>
+        <Text>Loading, please wait...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+      {movies.map((movie, index) => (
+        <MovieListItem movie={movie} key={index} />
+      ))}
+    </ScrollView>
+  );
+}
+
+function MovieListItem(props) {
+  let IMAGEPATH = 'http://image.tmdb.org/t/p/w500'
+  let imageurl = IMAGEPATH + props.movie.poster_path;
+  
+
+  return (
+    <View style={styles.movieItem}>
+      <View style={styles.movieItemImage}>
+        <Image source={{uri: imageurl}} style={{width: 99, height: 146}} />
+      </View>
+      <View style={{marginRight: 50}}>
+        <Text style={styles.movieItemTitle}>{props.movie.title}</Text>
+        <Text style={styles.movieItemText}>{props.movie.release_date}</Text>
+        <Text numberOfLines={6} ellipsizeMode="tail" style={styles.movieItemText}>{props.movie.overview}</Text>
+      </View> 
+    </View>
+  )
+}
+
+const MovieListScreen = ({ navigation }) => {
+    return (
+      <SafeAreaView>
+        <StatusBar/>
+        <View style={styles.sectionContainer}>
+          <Text>Hello React Native CLI</Text>
+        </View>
+        <MoviesList/>
+      </SafeAreaView>
+    );
+  };
+
+const styles = StyleSheet.create({
+  movieItem: {
+    margin: 5,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  movieItemImage: {
+    marginRight: 5
+  },
+  movieItemTitle: {
+    fontWeight: 'bold',
+  },
+  movieItemText: {
+    flexWrap: 'wrap'
+  }
+});
+
+export default MovieListScreen;
